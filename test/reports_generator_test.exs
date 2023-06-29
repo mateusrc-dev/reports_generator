7055b52cdbb9f744aa5ba17da944cd23fa341ffe
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-06-29 13:55:36 -0300

    created test to function build_from_many

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -69,9 +69,62 @@ defmodule ReportsGeneratorTest do
       response = ReportsGenerator.build_from_many(filenames)
 
       # this data got caught in the terminal after execution failed
-      expected_response = "banana"
+      expected_response =
+        {:ok,
+         %{
+           "foods" => %{
+             "açaí" => 2,
+             "churrasco" => 4,
+             "esfirra" => 6,
+             "hambúrguer" => 4,
+             "pastel" => 0,
+             "pizza" => 4,
+             "prato_feito" => 0,
+             "sushi" => 0
+           },
+           "users" => %{
+             "1" => 96,
+             "10" => 72,
+             "11" => 0,
+             "12" => 0,
+             "13" => 0,
+             "14" => 0,
+             "15" => 0,
+             "16" => 0,
+             "17" => 0,
+             "18" => 0,
+             "19" => 0,
+             "2" => 90,
+             "20" => 0,
+             "21" => 0,
+             "22" => 0,
+             "23" => 0,
+             "24" => 0,
+             "25" => 0,
+             "26" => 0,
+             "27" => 0,
+             "28" => 0,
+             "29" => 0,
+             "3" => 62,
+             "30" => 0,
+             "4" => 84,
+             "5" => 98,
+             "6" => 36,
+             "7" => 54,
+             "8" => 50,
+             "9" => 48
+           }
+         }}
 
       # ASSERTION
+      assert response == expected_response
+    end
+
+    test "when a file list is not provided, returns an error" do
+      response = ReportsGenerator.build_from_many("banana")
+
+      expected_response = {:error, "Please provide a list of strings"}
+
       assert response == expected_response
     end
   end
